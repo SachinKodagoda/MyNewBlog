7cbd98b79e1542e69157cea4a3ded86792f08301
Duminda Kodagoda <duminda.g.k@gmail.com>
2022-09-21 00:03:53 +0530

    image grid added

--- src/common/ui/layouts/Home/PoolBarSpa.tsx
+++ src/common/ui/layouts/Home/PoolBarSpa.tsx
@@ -1,39 +1,34 @@
 import styled from 'styled-components';
 
 import ScrollRef from '@components/ScrollRef';
+import { colors, devices, sizes } from '@theme/baseTheme';
 
 function PoolBarSpa(): JSX.Element {
   return (
     <Container>
       <ScrollRef name='Features' />
-      <Left>
+      <PhotoGrid>
         <Body>
           <BodyImg src='/images/home/img_5.jpg' alt='location' />
           <BodyDesc>X&apos;mas Parties</BodyDesc>
         </Body>
-      </Left>
-      <Right>
-        <RightFirst>
-          <Body>
-            <BodyImg src='/images/home/img_6.jpg' alt='location' />
-            <BodyDesc>Food</BodyDesc>
-          </Body>
-          <Body>
-            <BodyImg src='/images/home/img_7.jpg' alt='location' />
-            <BodyDesc>Drinks</BodyDesc>
-          </Body>
-        </RightFirst>
-        <RightSecond>
-          <Body>
-            <BodyImg src='/images/home/img_8.jpg' alt='location' />
-            <BodyDesc>Swimming</BodyDesc>
-          </Body>
-          <Body>
-            <BodyImg src='/images/home/img_9.jpg' alt='location' />
-            <BodyDesc>DJ</BodyDesc>
-          </Body>
-        </RightSecond>
-      </Right>
+        <Body>
+          <BodyImg src='/images/home/img_6.jpg' alt='location' />
+          <BodyDesc>Food</BodyDesc>
+        </Body>
+        <Body>
+          <BodyImg src='/images/home/img_7.jpg' alt='location' />
+          <BodyDesc>Drinks</BodyDesc>
+        </Body>
+        <Body>
+          <BodyImg src='/images/home/img_8.jpg' alt='location' />
+          <BodyDesc>Swimming</BodyDesc>
+        </Body>
+        <Body>
+          <BodyImg src='/images/home/img_9.jpg' alt='location' />
+          <BodyDesc>DJ</BodyDesc>
+        </Body>
+      </PhotoGrid>
     </Container>
   );
 }
@@ -44,53 +39,69 @@ export default PoolBarSpa;
 
 const Container = styled.div`
   height: auto;
-  padding: 0 16px;
+  padding: 0 ${sizes.md.px};
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
+  @media ${devices.minTabletLC} {
+    flex-direction: row;
+  }
 `;
 
-const Left = styled.div`
-  display: flex;
-  background: red;
+const PhotoGrid = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-areas:
+    'a'
+    'b'
+    'c'
+    'd'
+    'e';
+  @media ${devices.minTablet} {
+    grid-template-areas:
+      'a a'
+      'b c'
+      'd e';
+  }
+
+  @media ${devices.minTabletLC} {
+    grid-template-areas:
+      'a a b c'
+      'a a d e';
+  }
 `;
 
 const Body = styled.div`
   height: auto;
   position: relative;
+  &:nth-of-type(1) {
+    grid-area: a;
+  }
+  &:nth-of-type(2) {
+    grid-area: b;
+  }
+  &:nth-of-type(3) {
+    grid-area: c;
+  }
+  &:nth-of-type(4) {
+    grid-area: d;
+  }
+  &:nth-of-type(5) {
+    grid-area: e;
+  }
 `;
 
 const BodyImg = styled.img`
   width: 100%;
   height: auto;
   display: block;
-  background: #fff;
-  border: none;
 `;
 
 const BodyDesc = styled.div`
   position: absolute;
-  width: 180px;
-  height: 30px;
-  line-height: 30px;
-  font-size: 20px;
-  text-align: left;
-  padding-left: 10px;
-  left: 10px;
-  bottom: 10px;
-  color: #fff;
-`;
-
-const Right = styled.div`
-  display: flex;
-  flex-direction: column;
-  background: blue;
-`;
-
-const RightFirst = styled.div`
-  display: flex;
-`;
-
-const RightSecond = styled.div`
-  display: flex;
+  font-size: ${sizes.xl.f};
+  left: ${sizes.sm.px};
+  bottom: ${sizes.sm.px};
+  color: ${colors.white};
 `;
