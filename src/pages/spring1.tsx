71e4957ca4d6827b35a3b8559a88dd561220d8b6
Duminda Kodagoda <duminda.g.k@gmail.com>
2022-10-25 13:10:53 +0530

    clean ups

---
+++ src/pages/spring1.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+function Spring1(): JSX.Element {
+  return <Container>Spring1</Container>;
+}
+
+export default Spring1;
+
+const Container = styled.div``;
